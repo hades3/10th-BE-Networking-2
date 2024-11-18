dd535a847aa3d672b0a55ea6c103a7d7016cd443
Yeongchan Kim <hades1@kakao.com>
2024-11-19 00:26:04 +0900

    feat: Post Entity 구현

--- backend/src/main/java/cotato/backend/domains/post/Post.java
+++ backend/src/main/java/cotato/backend/domains/post/Post.java
@@ -1,13 +1,46 @@
 package cotato.backend.domains.post;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Getter
+@Setter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Post {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(nullable = false)
+	private String title;
+
+	@Column(nullable = false)
+	private String content;
+
+	@Column(nullable = false)
+	private String name;
+
+	@Column(nullable = false)
+	private int views;
+
+	public Post(String title, String content, String name) {
+		this.title = title;
+		this.content = content;
+		this.name = name;
+		this.views = 0;
+	}
+
+	public void increaseViews() {
+		this.views += 1;
+	}
 
 }
